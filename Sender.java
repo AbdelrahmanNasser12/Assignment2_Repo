7d9a23702ba376bb880a6f92c33461e7aec4e8ed
Ahmed <ahmedosama49@gmail.com>
2019-03-10 22:59:28 +0200

    keep sender thread

--- Sender.java
+++ Sender.java
@@ -1,19 +1,27 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Sender extends Thread {
+    Accepter accepter;
     Socket socket;
     public PrintWriter printWriter;
 
-    Sender(Socket socket) throws IOException {
+    Sender(Socket socket,Accepter accepter) throws IOException {
         this.socket=socket;
+        this.accepter=accepter;
         this.printWriter=new PrintWriter(socket.getOutputStream(), true);
     }
 
 
     public void run() {
-      
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            while (true) { bufferedReader.readLine(); }
+        }
+        catch (IOException e) {  }
     }
 
 
